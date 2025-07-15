20da4f0b739a508907e9be89e8258198752576ca
Benjamin Bomberg <b.bomberg@web.de>
2025-07-15 23:01:47 +0200

    first commit

---
+++ config/system/settings.php
@@ -0,0 +1,167 @@
+<?php
+return [
+    'BE' => [
+        'debug' => false,
+        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$RzNtaG43bS9KVUIzUDBOQg$8JlByh+9+z9at5FxR5Qh2QLwHSz6FSPf063OxbD8WxM',
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8mb4',
+                'dbname' => 'baum_und_menschdb',
+                'driver' => 'mysqli',
+                'host' => 'sql672.your-server.de',
+                'password' => 'uGZpdc4aDm48dbab',
+                'port' => 3306,
+                'tableoptions' => [
+                    'charset' => 'utf8mb4',
+                    'collate' => 'utf8mb4_unicode_ci',
+                ],
+                'user' => 'bonnog_1',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => '',
+            'loginBackgroundImage' => '',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => '',
+            'loginLogoAlt' => '',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+        'recaptcha' => [
+            'api_server' => 'https://www.google.com/recaptcha/api.js',
+            'enforceCaptcha' => '1',
+            'invisible_private_key' => '',
+            'invisible_public_key' => '',
+            'lang' => '',
+            'private_key' => '6LffdYQrAAAAAECPFDgfWhvphUPdsXeVU9053hpv',
+            'public_key' => '6LffdYQrAAAAAFCUD50Pz5I5Td14CfXNzUE1PPpT',
+            'robotMode' => '0',
+            'verify_server' => 'https://www.google.com/recaptcha/api/siteverify',
+        ],
+        'staticfilecache' => [
+            'backendDisplayMode' => 'both',
+            'boostMode' => '0',
+            'cacheTagsEnable' => '0',
+            'clearCacheForAllDomains' => '1',
+            'cspGenerationOverride' => '0',
+            'debugHeaders' => '0',
+            'disableInDevelopment' => '0',
+            'enableGeneratorBrotli' => '0',
+            'enableGeneratorGzip' => '1',
+            'enableGeneratorPhp' => '0',
+            'enableGeneratorPlain' => '0',
+            'hashUriInCache' => '0',
+            'headerSizeBuffer' => '1.5',
+            'htaccessTemplateName' => 'EXT:staticfilecache/Resources/Private/Templates/Htaccess.html',
+            'inlineAssetsFileSize' => '50000',
+            'inlineScriptMinify' => '0',
+            'inlineServiceFavIcon' => '0',
+            'inlineServiceScripts' => '0',
+            'inlineServiceStyles' => '0',
+            'inlineStyleAssets' => 'ico,png,woff2',
+            'inlineStyleMinify' => '0',
+            'largeIdentifierInCacheTable' => '0',
+            'maxHeaderSize' => '8192',
+            'overrideCacheDirectory' => '',
+            'overrideClientUserAgent' => '',
+            'phpTemplateName' => 'EXT:staticfilecache/Resources/Private/Templates/Php.html',
+            'rawurldecodeCacheFileName' => '0',
+            'renameTablesToOtherPrefix' => '0',
+            'sendCacheControlHeaderRedirectAfterCacheTimeout' => '1',
+            'sendHttp2PushEnable' => '0',
+            'sendHttp2PushFileExtensions' => 'css,js',
+            'sendHttp2PushFileLimit' => '10',
+            'sendHttp2PushLimitToArea' => '',
+            'useFallbackMiddleware' => '1',
+            'useReverseUriLengthInPriority' => '1',
+            'validFallbackHeaders' => 'Content-Type,Content-Language,Content-Security-Policy,Link,X-SFC-Tags',
+            'validHtaccessHeaders' => 'Content-Type,Content-Language,Content-Security-Policy,Link,X-SFC-Tags',
+        ],
+    ],
+    'FE' => [
+        'cacheHash' => [
+            'enforceValidation' => true,
+        ],
+        'debug' => false,
+        'disableNoCacheParameter' => true,
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'GFX' => [
+        'processor' => 'GraphicsMagick',
+        'processor_effects' => false,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/bin/',
+    ],
+    'LOG' => [
+        'TYPO3' => [
+            'CMS' => [
+                'deprecations' => [
+                    'writerConfiguration' => [
+                        'notice' => [
+                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                                'disabled' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'MAIL' => [
+        'transport' => 'sendmail',
+        'transport_sendmail_command' => '/usr/local/bin/mailpit sendmail -t --smtp-addr 127.0.0.1:1025',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => '',
+        'transport_smtp_username' => '',
+    ],
+    'SYS' => [
+        'UTF8filesystem' => true,
+        'caching' => [
+            'cacheConfigurations' => [
+                'hash' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+                'pages' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'rootline' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+            ],
+        ],
+        'devIPmask' => '',
+        'displayErrors' => 0,
+        'encryptionKey' => 'e166bb54c1dc09b7fc0961bfd53cbdaec367c6617927c529f94d348f0e944119d51f7fb0db92771c9cfa902b246f13cc',
+        'exceptionalErrors' => 4096,
+        'features' => [
+            'frontend.cache.autoTagging' => true,
+            'security.system.enforceAllowedFileExtensions' => true,
+        ],
+        'sitename' => 'Baum und Mensch DDEV',
+        'systemMaintainers' => [
+            1,
+        ],
+    ],
+];
---
+++ packages/cbw-sitepackage/Build/js/cbw_sitepackage.js
@@ -0,0 +1,232 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    /**
+     * Zoom for Devices with viewport width 320px
+     */
+    let g = document.documentElement.clientWidth;
+    let f = document.querySelector("meta[name=viewport]");
+
+    if (g < 768 && f) {
+        f.setAttribute("content", "width=360, maximum-scale=1.0, user-scalable=0");
+    }
+    if (g < 1280 && f) {
+        f.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=10.0,user-scalable=1');
+    }
+
+
+    // mobile menu starts
+    let $navIcon = document.getElementById('hamburger-menu');
+
+    if ($navIcon.offsetParent !== 0) {
+        let Closed = false;
+        let navMenu = document.getElementById('menu-main');
+        let menu = document.querySelector('.menu-main');
+
+        $navIcon.addEventListener("click", function () {
+            if (Closed) {
+                $navIcon.classList.remove('open');
+                $navIcon.classList.add('closed');
+                Closed = false;
+            } else {
+                $navIcon.classList.remove('closed');
+                $navIcon.classList.add('open');
+                Closed = true;
+            }
+            //menu.classList.toggle('d-none');
+
+            if (!navMenu.classList.contains('open')) {
+                navMenu.classList.add('open');
+                document.body.style.overflow = 'hidden';
+            }  else {
+                navMenu.classList.remove('open');
+                document.body.style.overflow = null;
+            }
+        });
+
+        let $subMenuOpener = document.querySelectorAll('.w-submenu__opener');
+
+        $subMenuOpener.forEach((subMenuOpener) => {
+            subMenuOpener.addEventListener('click', function () {
+                let mainSub = subMenuOpener.closest('.menu-main_item.lvl1').querySelector('.menu-main__sub.lvl2');
+
+                subMenuOpener.classList.toggle('open');
+                mainSub.classList.toggle('open');
+
+            });
+        });
+
+        let $menuMainActiveSwitcherMobile = document.querySelectorAll('.menu-main_item.lvl1.w-submenu');
+        for (let i = 0, len = $menuMainActiveSwitcherMobile.length; i < len; i++) {
+            if ($menuMainActiveSwitcherMobile[i].classList.contains('active')) {
+                $menuMainActiveSwitcherMobile[i].querySelector('.menu-main__sub.lvl2').classList.add('open');
+            } else {
+                $menuMainActiveSwitcherMobile[i].querySelector('.menu-main__sub.lvl2').classList.remove('open');
+            }
+        }
+
+    }
+    // mobile menu ends
+    document.addEventListener('scroll', function () {
+        let headerEl = document.querySelector(".header");
+        let body = document.body;
+        let scrollUp;
+        let scrollDown;
+        if (g < 768 && f) {
+            scrollDown = 80;
+            scrollUp = 100;
+        }
+        if (g > 1023 && f) {
+            scrollDown = 100;
+            scrollUp = 120;
+        }
+        if (typeof (headerEl) != 'undefined' && headerEl != null) {
+            headerScrollFunc(headerEl, body, scrollDown, scrollUp);
+        }
+    });
+
+    function headerScrollFunc(headerEl, body, scrollDown, scrollUp) {
+        if (document.body.scrollTop > scrollDown || document.documentElement.scrollTop > scrollUp) {
+            headerEl.classList.add("header-small");
+            body.classList.add("header-is-small");
+        } else {
+            headerEl.classList.remove("header-small");
+            body.classList.remove("header-is-small");
+        }
+    }
+
+    let slider = document.getElementById("splide");
+
+    if (typeof (slider) != 'undefined' && slider != null)
+    {
+        new Splide('#splide', {
+            type: 'loop',
+            perPage: 3,
+            perMove: 1,
+            autoplay: true,
+            interval: 8000,
+            updateOnMove: true,
+            pagination: false,
+            throttle: 300,
+            breakpoints: {
+                1440: {
+                    perPage: 2,
+                },
+                768: {
+                    perPage: 1,
+                }
+            }
+        }).mount();
+    }
+
+    let imageSlider = document.getElementById("imageSlide");
+
+    if (typeof (imageSlider) != 'undefined' && imageSlider != null)
+    {
+        new Splide('#imageSlide', {
+            type: 'loop',
+            perPage: 4,
+            perMove: 1,
+            autoplay: false,
+            interval: 8000,
+            updateOnMove: true,
+            pagination: false,
+            throttle: 300,
+            breakpoints: {
+                1440: {
+                    perPage: 1,
+                }
+            }
+        }).mount();
+    }
+
+    GLightbox({
+        touchNavigation: true,
+        loop: true,
+        autoplayVideos: false,
+        selector: ".glightbox"
+    });
+    const lightbox = GLightbox({
+        openEffect: "zoom",
+        closeEffect: "fade",
+        touchNavigation: true,
+        loop: true,
+        autoplayVideos: true
+    });
+
+
+    function setHeight(el, val) {
+        if (typeof val === "function") val = val();
+        if (typeof val === "string") el.style.height = val;
+        else el.style.height = val + "px";
+    }
+
+    let equalheight = function(container){
+        let currentTallest = 0,
+            currentRowStart = 0,
+            rowDivs = new Array(),
+            $el,
+            topPosition = 0;
+
+        Array.from(document.querySelectorAll(container)).forEach((el,i) => {
+            el.style.height = "auto";
+            let topPosition = el.offsetTop;
+            let currentDiv;
+            if(currentRowStart !== topPosition){
+                for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                    setHeight(rowDivs[currentDiv], currentTallest);
+                }
+                rowDivs.length = 0;
+                currentRowStart = topPosition;
+                currentTallest = parseFloat(getComputedStyle(el, null).height.replace("px", ""));
+                rowDivs.push(el);
+            } else {
+                rowDivs.push(el);
+                currentTallest = (currentTallest < parseFloat(getComputedStyle(el, null).height.replace("px", ""))) ? (parseFloat(getComputedStyle(el, null).height.replace("px", ""))) : (currentTallest);
+            }
+            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                setHeight(rowDivs[currentDiv], currentTallest);
+            }
+        });
+    };
+    if (g >= 768) {
+        window.addEventListener("load", function(){
+            equalheight('.content-column-item p');
+        });
+        window.addEventListener("resize", function(){
+            setTimeout(function(){
+                equalheight('.content-column-item p');
+            });
+        });
+    }
+
+    let privacy = document.querySelector('input.declaration');
+    if (typeof (privacy) != 'undefined' && privacy != null)
+    {
+        privacy.addEventListener('change', (event) => {
+            if (privacy.closest('.input').classList.contains("invalid")) {
+                privacy.closest(".input").classList.remove("invalid");
+            }
+        });
+    }
+});
+
+
+function myCallBack() {
+    let recaptcha1;
+    let recaptcha2;
+    let recaptchaBox1 = document.getElementById('recaptcha-1');
+    let recaptchaBox2 = document.getElementById('recaptcha-2');
+
+    if (typeof (recaptchaBox1) != 'undefined' && recaptchaBox1 != null)
+    {
+        recaptcha1 = grecaptcha.render(document.getElementById('recaptcha-1'), {
+            'sitekey' : '6Lf9U-smAAAAAJiSuBFiQLxsyo6KKW0FnJFsP-XG'
+        });
+    }
+
+    if (typeof (recaptchaBox2) != 'undefined' && recaptchaBox2 != null)
+    {
+        recaptcha2 = grecaptcha.render(document.getElementById('recaptcha-2'), {
+            'sitekey' : '6LfjiWIpAAAAAHDVISXWEodsfLxN6WJmUhm9KFQF'
+        });
+    }
+}
